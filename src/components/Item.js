7d21308b6b96442d9ee86895fe04ceef17a0eff1
Habsa Abdirizack <habsa.abdirizack@student.moringaschool.com>
2024-11-08 10:18:43 +0300

    assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React,{ useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] =useState(false);
+  const toggleCart =() => {
+    setInCart((prev) => !prev);
+  };
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : "" }>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={toggleCart}>
+        {inCart ? "Remove from Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
+
 
 export default Item;
